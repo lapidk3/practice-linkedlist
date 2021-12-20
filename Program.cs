4c4f60695c3ea3696483f48111f577d0d7819743
Kenneth David <lapidk3@yahoo.com>
2021-12-20 13:43:48 -0800

    A simple linkedlist class

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class MyLinkedList
+    {
+        Node head;
+
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int d) 
+            {
+                data = d;
+                next = null;
+            }
+        }
+    }
+}
